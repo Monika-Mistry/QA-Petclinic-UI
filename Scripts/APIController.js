2c5748cc7524e0806ed56864b1ec82bc2c6cd37a
Chris McQuiggan <chris@mcquiggan.info>
2019-06-13 11:59:36 +0100

    moved working file location

--- Scripts/APIController.js
+++ Scripts/APIController.js
@@ -31,35 +31,38 @@ function promises(req) {
 }
 
 function resolved(result) {
-    if (result.id === undefined) {
+    if (result[0] !== undefined || result.id !== undefined) {
+        //create table tags
+        let node = document.createElement("tbody");
+        node.setAttribute("id", "treturn");
+        document.getElementById("results").appendChild(node);
+        let tr = "<tr>";
+
         if (result[0] !== undefined) {
-            for (let c in result) {
+            for (let i = 0; i < result.length; i++) {
                 //output to table
-                let output;
-                let node = document.createElement("div");
-                let textnode = document.createTextNode(output);
-                node.setAttribute("id", "return" + c);
-                node.appendChild(textnode);
-                document.getElementById("results").appendChild(node);
+                let petstring = "";
+                for (let j = 0; j < result.pets.length; j++) {
+                    petstring += result.pets[j].name + ", ";
+                }
+                tr += "<td>" + result[i].id + "</td><td>$" + result[i].firstName + "</td><td>$" + result[i].lastName + "</td><td>$" + result[i].address + "</td><td>$" + result[i].city + "</td><td>$" + result[i].telephone + "</td><td>$" + petstring + "</td></tr>";
+                tbody.innerHTML += tr;
             }
         } else {
-            //message output (deleted object)
-            let output;
-            let node = document.createElement("div");
-            node.setAttribute("id", "return");
-            node.appendChild(textnode);
-            document.getElementById("results").appendChild(node);
+            let petstring = "";
+            for (let j = 0; j < result.pets.length; j++) {
+                petstring += result.pets[j].name + ", ";
+            }
+            tr += "<td>" + result[i].id + "</td><td>$" + result[i].firstName + "</td><td>$" + result[i].lastName + "</td><td>$" + result[i].address + "</td><td>$" + result[i].city + "</td><td>$" + result[i].telephone + "</td><td>$" + petstring + "</td></tr>";
+            tbody.innerHTML += tr;
         }
     } else {
-        //output for 1 result
-        let output;
-        let textnode = document.createTextNode(output);
+        let textnode = document.createTextNode("Success!");
         let node = document.createElement("div");
         node.setAttribute("id", "return");
         node.appendChild(textnode);
         document.getElementById("results").appendChild(node);
     }
-
 }
 
 function rejected(reason) {
