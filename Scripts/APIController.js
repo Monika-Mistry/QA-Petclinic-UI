53b58dbcb3aa57d1a38e9981ff0958689636bd6c
Chris McQuiggan <chris@mcquiggan.info>
2019-06-13 13:19:17 +0100

    formated table

--- Scripts/APIController.js
+++ Scripts/APIController.js
@@ -32,20 +32,22 @@ function resolved(result) {
     if (result[0] !== undefined || result.id !== undefined) {
         //create table tags
         let node = document.createElement("tbody");
-        node.setAttribute("id", "treturn");
+        node.setAttribute("id", "tbody");
         document.getElementById("results").appendChild(node);
         let tr = "<tr>";
-
+        tr += "<td>|-- ID --|</td><td>|---- First Name ----|</td><td>|---- Last Name ----|</td><td>|---------- Address ----------|</td><td>|------ City ------|</td><td>|---- Telephone Number ----|</td><td>|-------- Pets --------|</td></tr>";
+        // tbody.innerHTML += tr;
         if (result[0] !== undefined) {
             for (let i = 0; i < result.length; i++) {
                 //output to table
                 let petstring = "";
-                for (let j = 0; j < result.pets.length; j++) {
-                    petstring += result.pets[j].name + ", ";
+                for (let j = 0; j < result[i].pets.length; j++) {
+                    petstring += result[i].pets[j].name + ", ";
                 }
-                tr += "<td>" + result[i].id + "</td><td>$" + result[i].firstName + "</td><td>$" + result[i].lastName + "</td><td>$" + result[i].address + "</td><td>$" + result[i].city + "</td><td>$" + result[i].telephone + "</td><td>$" + petstring + "</td></tr>";
-                tbody.innerHTML += tr;
+                tr += "<td>" + result[i].id + "   </td><td>" + result[i].firstName + "   </td><td>" + result[i].lastName + "   </td><td>" + result[i].address + "   </td><td>" + result[i].city + "   </td><td>" + result[i].telephone + "   </td><td>" + petstring + "   </td></tr>";
+                // tbody.innerHTML += tr;
             }
+            tbody.innerHTML += tr;
         } else {
             let petstring = "";
             for (let j = 0; j < result.pets.length; j++) {
@@ -66,4 +68,10 @@ function resolved(result) {
 function rejected(reason) {
     alert("An Error occured. Please check input.\n" + reason);
     console.log(reason);
+}
+
+function testClick(){
+    let type = "GET";
+    let url = "http://localhost:9966/petclinic/api/owners";
+    makeRequest(type, url)
 }
